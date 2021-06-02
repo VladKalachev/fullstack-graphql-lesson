318321e101e0265a4fea629bae050b10dc1fd26d
Corvax <vlvete1990@yandex.ru>
2021-06-02 22:57:04 +0300

    chore: added entities Post

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello Horld');
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+
+const main = async () => {
+    const orm =  await MikroORM.init({
+        entities: [Post],
+        dbName: 'lireddit',
+        type: 'postgresql',
+        debug: !__prod__,
+    });
+};
+
+
+console.log('Hello World');
