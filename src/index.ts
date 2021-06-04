8f0dd0676eca6cbe538e5539c75113fbc5fa60ca
Corvax <vlvete1990@yandex.ru>
2021-06-04 20:09:04 +0300

    chore: added find Post

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { MikroORM } from '@mikro-orm/core';
-import { Post } from './entities/Post';
+// import { Post } from './entities/Post';
 import mikroConfig from './mikro-orm.config';
 
 const main = async () => {
     const orm =  await MikroORM.init(mikroConfig);
+    await orm.getMigrator().up();
+    // const post = orm.em.create(Post, { title: 'mt first post' });
+    // await orm.em.persistAndFlush(post);
 
-    const post = orm.em.create(Post, { title: 'mt first post' });
-    await orm.em.persistAndFlush(post);
-    console.log('--------------sql 2------------------');
-    await orm.em.nativeInsert(Post, {title: 'mt first post 2'});
+    // const posts = await orm.em.find(Post, {});
+    // console.log(posts)
 };
 
 main().catch((err) => {
