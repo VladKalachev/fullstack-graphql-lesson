17408014b3f78e38804bc8bcaf6d99446ca7fca9
Corvax <vlvete1990@yandex.ru>
2021-06-04 22:08:03 +0300

    chore: added get users and user

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import { User } from '../entities/User';
 import { MyContext } from '../types';
-import { Resolver, Mutation, InputType, Field, Arg, Ctx} from 'type-graphql';
+import { Resolver, Mutation, InputType, Field, Arg, Ctx, Query, Int} from 'type-graphql';
 import argon2 from 'argon2';
 
 @InputType()
@@ -13,6 +13,22 @@ class UsernamePasswordInput {
 
 @Resolver()
 export class UserResolver {
+
+  @Query(() => [User])
+  users( 
+    @Ctx() {em}: MyContext
+  ): Promise<User[]> {
+    return em.find(User, {});
+  }
+
+  @Query(() => User, { nullable: true })
+  user(
+    @Arg('id', () => Int) id: number,
+    @Ctx() {em}: MyContext
+  ): Promise<User | null> {
+    return em.findOne(User, { id });
+  }
+
   @Mutation(() => User)
   async register(
     @Arg('options') options: UsernamePasswordInput,
