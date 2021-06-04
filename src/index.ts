37b63ffac5a83f23b7ea134d841e1d8f4e015599
Corvax <vlvete1990@yandex.ru>
2021-06-04 20:16:09 +0300

    chore: added express

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import { MikroORM } from '@mikro-orm/core';
 // import { Post } from './entities/Post';
 import mikroConfig from './mikro-orm.config';
+import express, {Request, Response} from 'express';
 
 const main = async () => {
     const orm =  await MikroORM.init(mikroConfig);
     await orm.getMigrator().up();
-    // const post = orm.em.create(Post, { title: 'mt first post' });
-    // await orm.em.persistAndFlush(post);
-
-    // const posts = await orm.em.find(Post, {});
-    // console.log(posts)
+    const app = express();
+    app.get('/', (_: Request, res: Response) => {
+        res.send('Hello');
+    });
+    app.listen(4000, () => {
+        console.log('Server started on localhost:4000')
+    })
 };
 
 main().catch((err) => {
