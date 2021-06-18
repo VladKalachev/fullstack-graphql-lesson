feb0e2e37a0770bc59dac7a0113a6c835ccd6892
Corvax <vlvete1990@yandex.ru>
2021-06-18 20:16:32 +0300

    chore: added middleware

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -4,9 +4,11 @@ import {
   PrimaryGeneratedColumn, 
   CreateDateColumn, 
   UpdateDateColumn, 
-  BaseEntity
+  BaseEntity,
+  ManyToOne,
 } from "typeorm";
 import { ObjectType, Field } from "type-graphql";
+import { User } from "./User";
 
 @ObjectType()
 @Entity()
@@ -15,6 +17,25 @@ export class Post extends BaseEntity {
   @PrimaryGeneratedColumn()
   id!: number;
 
+  @Field()
+  @Column()
+  title!: string;
+
+  @Field()
+  @Column()
+  text!: string;
+
+  @Field()
+  @Column({ type: "int", default: 0 })
+  points!: number;
+
+  @Field()
+  @Column()
+  creatorId: number;
+
+  @ManyToOne(() => User, user => user.posts)
+  creator: User;
+
   @Field(() => String)
   @CreateDateColumn()
   createdAt: Date;
@@ -22,8 +43,5 @@ export class Post extends BaseEntity {
   @Field(() => String)
   @UpdateDateColumn()
   updatedAt: Date;
-
-  @Field(() => String)
-  @Column()
-  title!: string;
+  
 }
