7af024a08877b9d5c62d0bba6701ab0f9953f801
Corvax <vlvete1990@yandex.ru>
2021-07-28 21:56:44 +0300

    chore: added query builder posts

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -33,25 +33,55 @@ export class PostResolver {
     @Arg('cursor', () => String, { nullable: true }) cursor: string | null
   ): Promise<PaginatedPosts> {
     const realLimit = Math.min(50, limit);
-    const realLimitPlusOne = realLimit + 1;
-    const qb = getConnection()
-    .getRepository(Post)
-    .createQueryBuilder('p')
-    .orderBy('"createdAt"', 'DESC')
-    .take(realLimitPlusOne);
+    // const realLimitPlusOne = realLimit + 1;
+    const reaLimitPlusOne = realLimit + 1;
+    const replacements: any[] = [reaLimitPlusOne];
 
-    if(cursor) {
-      qb.where('"createdAt" < :cursor', { 
-        cursor: new Date(parseInt(cursor))
-      });
+    if (cursor) {
+      replacements.push(new Date(parseInt(cursor)));
     }
 
-    const posts = await qb.getMany();
+    const posts = await getConnection().query(
+      `
+    select p.*,
+    json_build_object(
+      'id', u.id,
+      'username', u.username,
+      'email', u.email
+      ) creator
+    from post p
+    inner join public.user u on u.id = p."creatorId"
+    ${cursor ? `where p."createdAt" < $2` : ""}
+    order by p."createdAt" DESC
+    limit $1
+    `,
+      replacements
+    );
+  
+    // const qb = getConnection()
+    // .getRepository(Post)
+    // .createQueryBuilder('p')
+    // .innerJoinAndSelect("p.creator", "u", 'u.id = p."creatorId"')
+    // .orderBy('p."createdAt"', 'DESC')
+    // .take(realLimitPlusOne);
 
+    // if(cursor) {
+    //   qb.where('p."createdAt" < :cursor', { 
+    //     cursor: new Date(parseInt(cursor))
+    //   });
+    // }
+
+    // const posts = await qb.getMany();
+
+    // console.log("posts ", posts);
     return {
       posts: posts.slice(0, realLimit),
-      hasMore: posts.length === realLimitPlusOne,
+      hasMore: posts.length === reaLimitPlusOne,
     };
+    // return {
+    //   posts: posts.slice(0, realLimit),
+    //   hasMore: posts.length === realLimitPlusOne,
+    // };
   }
 
   @Query(() => Post, { nullable: true})
