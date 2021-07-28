a10309922d07e1bf332fede841bf9ef7de0d52cf
Corvax <vlvete1990@yandex.ru>
2021-07-28 21:07:39 +0300

    chore: fix

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,6 +1,6 @@
 import { Post } from '../entities/Post';
 import { getConnection } from 'typeorm';
-import { Resolver, Query, Arg, Int, Mutation, InputType, Field, Ctx, UseMiddleware, FieldResolver, Root } from 'type-graphql';
+import { Resolver, Query, Arg, Int, Mutation, InputType, Field, Ctx, UseMiddleware, FieldResolver, Root, ObjectType } from 'type-graphql';
 import { MyContext } from '../types';
 import { isAuth } from '../middleware/isAuth';
 
@@ -12,6 +12,14 @@ class PostInput {
   text: string;
 }
 
+@ObjectType()
+class PaginatedPosts {
+  @Field(() => [Post])
+  posts: Post[]
+  @Field()
+  hasMore: boolean;
+}
+
 @Resolver(Post)
 export class PostResolver {
   @FieldResolver(() => String)
@@ -19,17 +27,18 @@ export class PostResolver {
     return root.text.slice(0, 50)
   }
 
-  @Query(() => [Post])
+  @Query(() => PaginatedPosts)
   async posts(
     @Arg('limit', () => Int) limit: number,
     @Arg('cursor', () => String, { nullable: true }) cursor: string | null
-  ): Promise<Post[]> {
+  ): Promise<PaginatedPosts> {
     const realLimit = Math.min(50, limit);
+    const realLimitPlusOne = realLimit + 1;
     const qb = getConnection()
     .getRepository(Post)
     .createQueryBuilder('p')
     .orderBy('"createdAt"', 'DESC')
-    .take(realLimit);
+    .take(realLimitPlusOne);
 
     if(cursor) {
       qb.where('"createdAt" < :cursor', { 
@@ -37,7 +46,12 @@ export class PostResolver {
       });
     }
 
-    return qb.getMany();
+    const posts = await qb.getMany();
+
+    return {
+      posts: posts.slice(0, realLimit),
+      hasMore: posts.length === realLimitPlusOne,
+    };
   }
 
   @Query(() => Post, { nullable: true})
